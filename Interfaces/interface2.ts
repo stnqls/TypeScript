d77ddd432304f24deb037b744334438e38d5d361
subin <stnqls3938@gmail.com>
2023-02-15 20:35:56 +0900

    Optional property

---
+++ Interfaces/interface2.ts
@@ -0,0 +1,39 @@
+interface Person2 {
+  name: string;
+  age?: number; // 있을수도 있고, 없을수도 있는경우 '?'를 사용한다.
+}
+
+function hello2(person: Person2): void {
+  console.log(`2번 ${person.name} 입니다.`);
+}
+
+hello2({ name: "Mark", age: 20 });
+hello2({ name: "Anna" });
+
+interface Person3 {
+  name: string;
+  age?: number;
+  [index: string]: any; // 어떤이름과 형식의 프로퍼티가 추가되어도 상관이 없다.
+}
+
+function hello3(person: Person3): void {
+  console.log(`안녕하세요 3번 ${person.name} 입니다.`);
+}
+
+const p31: Person3 = {
+  name: "Mark",
+  age: 30,
+};
+
+const p32: Person3 = {
+  name: "Anna",
+  systers: ["Sung", "Chan"],
+};
+
+const p33: Person3 = {
+  name: "Bokdaengi",
+  father: p31,
+  mother: p32,
+};
+
+hello3(p33);
