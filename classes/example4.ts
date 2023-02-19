508510cd57770f462ff7e6e9be63a91d308330a0
subin <stnqls3938@gmail.com>
2023-02-19 15:51:09 +0900

    Singletons

--- classes/example4.ts
+++ classes/example4.ts
@@ -17,3 +17,25 @@ p_c3.change();
 // Person_c3.hello();
 Person_c3.city;
 // 클래스로 만들어진 object로 부터 공통적으로 사용하고싶은 데이터가 있을경우 static을 사용한다.
+
+// Singletons
+class ClassName {
+  private static instance: ClassName | null = null;
+
+  public static getInstance(): ClassName {
+    // ClassName으로부터 만든 object가 있으면 그걸 리턴하고, 없으면 만들어서 리턴한다.
+    if (ClassName.instance === null) {
+      ClassName.instance = new ClassName();
+    }
+
+    return ClassName.instance;
+  }
+
+  private constructor() {}
+}
+
+// const A = new ClassName() constructor가 private이기때문에 오류가 발생한다.
+const B = ClassName.getInstance();
+const C = ClassName.getInstance();
+
+console.log(B === C); // TRUE
