28f69b2fa22388f03db7b2c4cdd1994d8f56b2f5
subin <stnqls3938@gmail.com>
2023-02-19 16:53:52 +0900

    Generics extends

--- generic/generic4.ts
+++ generic/generic4.ts
@@ -27,3 +27,16 @@ class genericPerson<T, K> {
 
 new genericPerson("Mark", 10);
 new genericPerson<number, string>(20, "Mark");
+
+//T는 string또는 number만 사용 가능하게 제한해준다.
+class PersonExtends<T extends string | number> {
+  private _name: T;
+
+  constructor(name: T) {
+    this._name = name;
+  }
+}
+
+new PersonExtends("Mark");
+new PersonExtends(30);
+// new PersonExtends(false); error!
